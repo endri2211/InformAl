a813523387a9a3aa2b88ed9aa1cb9b1849c23ea9
henriune <irnehnovi@gmail.com>
2020-10-02 14:02:17 +0200

    Updates

---
+++ precache-manifest.f182f6f121f6040aa81a9be92187e1fc.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cb1bae61a67a1e7594b",
+    "url": "/InformAl/static/js/app.07e5e73b.chunk.js"
+  },
+  {
+    "revision": "4a5a2fcbae9c295ebe3c",
+    "url": "/InformAl/static/js/runtime~app.ba78eca5.js"
+  },
+  {
+    "revision": "c836618c3d74a57cdcbc",
+    "url": "/InformAl/static/js/2.b908238a.chunk.js"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/InformAl/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "53e2eaf02aff20393d74992d3f1847a7",
+    "url": "/InformAl/static/media/flame.4b4bfc48.png"
+  },
+  {
+    "revision": "e577a1e6468d0c91b1128086cf1db9c4",
+    "url": "/InformAl/static/media/12_daniel_can_bc-207.44511b1d.jpg"
+  },
+  {
+    "revision": "b87ada8b17e41d736632d3d4fb396861",
+    "url": "/InformAl/static/media/emo-happiness.55e876ff.jpg"
+  },
+  {
+    "revision": "8ef760285b8ab7d8785572d040b8c474",
+    "url": "/InformAl/static/media/nature1_gettyimages.b3298ea1.jpg"
+  },
+  {
+    "revision": "147d63a05582ae4bc6fc67ee237e33b8",
+    "url": "/InformAl/static/media/96d0a32e8ab168eac62d8beee2ffabf7.17.4e1d41a6.jpg"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/InformAl/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/InformAl/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/InformAl/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/InformAl/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/InformAl/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/InformAl/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/InformAl/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/InformAl/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/InformAl/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/InformAl/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/InformAl/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "fcadfc4fafec5fd8e176423cf60a773a",
+    "url": "/InformAl/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/InformAl/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/InformAl/expo-service-worker.js"
+  },
+  {
+    "revision": "57fceeca84d98040cd9295309539edd1",
+    "url": "/InformAl/register-service-worker.js"
+  },
+  {
+    "revision": "29d88fb795ad3112d52869eaeb5899cf",
+    "url": "/InformAl/static/js/2.b908238a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b84a2006e920be0b658962a4f90c88",
+    "url": "/InformAl/favicon-16.png"
+  },
+  {
+    "revision": "cf42f508e7212b9a041ba4afacd251cf",
+    "url": "/InformAl/favicon-32.png"
+  },
+  {
+    "revision": "6a1741f7e399fc58567fa25407b37dda",
+    "url": "/InformAl/favicon.ico"
+  },
+  {
+    "revision": "8ec25c4dea32f00639b6c6a810699a3a",
+    "url": "/InformAl/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/InformAl/expo-service-worker.js",{scope:"/InformAl/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
